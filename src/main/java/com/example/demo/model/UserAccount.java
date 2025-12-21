ac47538953e244bbd1f01031d6b38c3c2c1f4438
bala-amypo <bala@amypo.in>
2025-12-21 08:33:02 +0000

    User commit

--- src/main/java/com/example/demo/model/UserAccount.java
+++ src/main/java/com/example/demo/model/UserAccount.java
@@ -2,20 +2,14 @@ package com.example.auth.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 
 @Entity
-@Table(
-    name = "user_account",
-    uniqueConstraints = @UniqueConstraint(columnNames = "email")
-)
 public class UserAccount {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
     private String fullName;
 
     @Email
