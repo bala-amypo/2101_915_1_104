75ba4871dec9366bf3a9d0e1561a41217ebe7e19
bala-amypo <bala@amypo.in>
2025-12-24 06:45:36 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,11 +1,7 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.List;
@@ -15,14 +11,10 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("Employee Equipment Issuance API").version("1.0"))
-                // Hardcode the server URL to port 9114 to prevent Swagger from guessing incorrectly
-                .servers(List.of(new Server().url("http://localhost:9114").description("Local Dev Server")))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")));
+                .servers(List.of(
+                    // Try empty URL first - this makes Swagger use the URL in your address bar
+                    new Server().url("/").description("Default Server"),
+                    new Server().url("http://localhost:9114").description("Local Port")
+                ));
     }
 }
