41db1a1d8669c37a1fd5b109a22a8950a4d41a28
bala-amypo <bala@amypo.in>
2025-12-26 10:34:27 +0000

    User commit

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -11,6 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
